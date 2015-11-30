5addb9acc23cdc340c6fd8ded630dca35b1a10c9
trinker <tyler.rinker@gmail.com>
2015-11-30 14:44:22 -0500

    removed tryCatch stuff from vc_column_apply

--- R/vc_column_apply.R
+++ R/vc_column_apply.R
@@ -10,33 +10,15 @@ vc_column_apply <- function(data, colmap){
     ## only check the headers that exist in both map and data
     map <- colmap[colnames(data)[colnames(data) %in% names(colmap)]]
     data <- data[names(map)]
-# tryCatch({
 
     Map(function(x, y, z){
 
         y <- gsub("\\)$", paste0(", colnames = \"", z, "\")"), y)
         results <- unlist(invisible(lapply(y, function(w) {
-
-            # tryCatch({
-
             eval(parse(text=w))
-#
-#             }, warning = function(w) {
-#     browser()
-# }, error = function(e) {
-#     browser()
-# })
-
             })))
         return(all(results))
 
     }, data, map, colnames(data))
-
-
-# }, warning = function(w) {
-#     browser()
-# }, error = function(e) {
-#     browser()
-# })
 
 }
