6394fcf59198da68b9e251dfa8f4076931a55580
Steve Simpson <steventroysimpson@gmail.com>
2016-01-05 14:54:55 -0500

    fixing column-wise testing

--- R/vc_required_condition.R
+++ R/vc_required_condition.R
@@ -1,17 +1,21 @@
 vc_required_condition <- function(x, colname_x = "The Empty Column", data){
 
     non_responses <- vc_non_response(x)
-    if (length(non_responses[["locations"]]) !=0){
+
+    # Steve 1/5/2016 We set length >1 because ParentIdentifier can be
+    # empty when is it at institution level.
+    # since vt_required is also reporting on it, we thought this more clarified
+    if (length(non_responses[["locations"]]) >1){
 
         institutions <- which(data[["Type"]] %in% c("institution", "Institution"))
 
         missings <- setdiff(non_responses[["locations"]], institutions)
 
 		message <- sprintf(
-			"The following report is actually an error if and only if \n
-			the column causing the error is not ParentIdentifier OR\n
-			it is ParentIdentifier and the missing observations aren't Instituion Level.\n\n
-			The following rows of column %s are required and missing:\n\n%s\n\n\n\n",
+			paste0("The following report is actually an error if and only if \n",
+			"the column causing the error is not ParentIdentifier OR\n",
+			"it is ParentIdentifier and the missing observations aren't Instituion Level.\n\n",
+			"The following rows of column %s are required and missing:\n\n%s\n\n\n\n"),
 			sQuote(colname_x)
 			, paste(missings
 					,collapse=", "))
