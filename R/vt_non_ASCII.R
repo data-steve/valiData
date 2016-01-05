55b68d34acde8749f654b49f6c85b79e0959c2a3
Steve Simpson <steventroysimpson@gmail.com>
2016-01-05 11:34:31 -0500

    added non-ASCII detection

--- R/vt_non_ASCII.R
+++ R/vt_non_ASCII.R
@@ -6,11 +6,11 @@
 #' @export
 vt_non_ASCII <- function(data,...){
 
-    is_non_ASCII <- sapply(data2, function(x) any(grepl("[[:cntrl:]]", suppressWarnings(stringi::stri_enc_toascii(x)))))
+    is_non_ASCII <- sapply(data, function(x) any(grepl("[[:cntrl:]]", suppressWarnings(stringi::stri_enc_toascii(x)))))
 
     if (any(is_non_ASCII)){
         which_hv_nonASCII <- names(is_non_ASCII[is_non_ASCII])
-        where_is_nonASCII <- lapply(data2[which_hv_nonASCII], function(x) grep("[[:cntrl:]]", suppressWarnings(stringi::stri_enc_toascii(x)) ))
+        where_is_nonASCII <- lapply(data[which_hv_nonASCII], function(x) grep("[[:cntrl:]]", suppressWarnings(stringi::stri_enc_toascii(x)) ))
         message <- paste0(
             "The following rows of "
             , sQuote(which_hv_nonASCII)
