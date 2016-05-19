5a58c0c666ed02e1a652c8a5d18c8b4702a87177
Tyler Rinker <trinker@campuslabs.com>
2016-05-19 15:33:36 -0400

    added printing to vd_dir (it is now a first class citizen)

--- R/vd_dir.R
+++ R/vd_dir.R
@@ -5,11 +5,38 @@
 #' @param path path in directory
 #' @param \ldots ignored.
 #' @export
-vd_dir <- function(path, ...) {  #not currently used/checked in valiData (should this be added?)
-	list(
+#' vd_dir(system.file())
+#' vd_dir(system.file("INDEX"))
+vd_dir <- function(path, ...) {
+	out <- list(
 		valid = file.info(path)[["isdir"]],
+	    message = paste0(
+	        "The following path is not a dirrectory:\n\n\t-",
+            path,
+            "\n\nCurrently, `valiData` operates on a directory with files -OR- a ",
+            "directory of sub-directories containing files\n\n\n\n"
+        ),
 		locations = NULL,
 		call = "vd_dir",
 		path = basename(path)
 	)
+	class(out) <- 'vd_dir'
+	out
+}
+
+
+
+#' Prints a vd_dir Object
+#'
+#' Prints a vd_dir object
+#'
+#' @param x A vd_dir object.
+#' @param \ldots ignored.
+#' @method print vd_dir
+#' @export
+print.vd_dir <- function (x, ...)	{
+
+	if (!isTRUE(x[["valid"]])) {
+		cat(x[['message']])
+	}
 }
