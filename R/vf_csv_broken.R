1003a1c4956309965e74ca1fcf319a7d340b7b06
Tyler Rinker <trinker@campuslabs.com>
2016-05-23 16:24:18 -0400

    vf_csv_broken was not handling added columns (user clicked in the csv after real columns) correctly because of how readr now handles (distinguishes them) from a comma broken csv.  This checking for added columns has been removed from vf_csv_broken and would be caught by vc_column_names and vc_enough_columns

--- R/vf_csv_broken.R
+++ R/vf_csv_broken.R
@@ -16,6 +16,13 @@
 #' vf_csv_broken(loc)
 vf_csv_broken <- function(path, ...){
 
+    ## Note: This function used to test if there were additional columns of
+    ## NAs with NA as column name that were created by a user manually inspecting
+    ## a csv and accidentally clicking to the right of true columns and thus
+    ## making new, unintended columns.  This used to be an error that readr did
+    ## not parse differently from comma broken csv files.  readr now handles
+    ## these types of csv file appropriately.
+
 	offender_rows <- NULL
 	cols  <- NULL
 	proportion <- NULL
@@ -24,68 +31,78 @@ vf_csv_broken <- function(path, ...){
 
     problem_cases <- readr::problems(data)
 
-    if (!is.null(problem_cases[["expected"]])) {
-        offending_problems <- grepl("^\\d+\\s*columns$", problem_cases[["expected"]])
-        if (any(offending_problems)) {
-	        offender_rows <- problem_cases[["row"]][offending_problems]
-        }
+    ## The broken csv check works under the assumption that readr's `problem`
+    ## function outputs a check with an expected and actual columns.  Broken
+    ## csv files have a readr `problems` output that looks like this:
+
+        ## > m <- readr::read_csv('Book1.csv')
+        ## Warning: 2 parsing failures.
+        ## row col  expected    actual
+        ##   3  -- 6 columns 7 columns
+        ##   5  -- 6 columns 1 columns
+
+    ## Under the assumption that broken csv files will have a differening number
+    ## expected and actual columns and that readr will report them in this
+    ## fashion we can test for broken csv files by looking for `problems` output
+    ## that have an expected column with the text "# columns" using the grep:
+    ## "^\\d+\\s*columns$".
+    ##
+    ## This depends on readr always reporting broken csv files in this way.
+    ##
+    ## Known limitations:
+    ##   1. This method will not catch broken csv files where there are
+    ##      additional empty columns and the comma break pushes content into
+    ##      these columns.
+
+    ## First if/else is looking for (based on assumptions stated above):
+    ##  1. readr `problems` function output problems w/ an 'expected' column
+    ##  2. Does the 'expected' column contain the pattern "# columns"
+    if (
+            ## the readr problems has an 'expected' column
+            !is.null(problem_cases[["expected"]]) &&
+
+            ## the readr problems's expected column contains the word column
+            any(grepl("^\\d+\\s*columns$", problem_cases[["expected"]]))
+        ) {
+
+            ## Check what elements of 'expected' column have "# columns" form
+            offending_problems <- grepl("^\\d+\\s*columns$", problem_cases[["expected"]])
+            ## Extract offending rows (index the grep from offending problems on the
+            ## 'row' column from `problems` output)
+            offender_rows <- problem_cases[["row"]][offending_problems]
+
+            ## find any columns that could be considered character class
+            cols <- candidates_for_quotes(path)
+            valid <- FALSE
+            error <- "empty-column"
+            offender_rows <- problem_cases[["row"]][offending_problems]
+            proportion <- 1 - length(offender_rows)/nrow(data)
+
+	        message <- sprintf(
+    	            paste0(header("Broken CSV Test"),
+                   "The file '%s' appears to be a broken csv with about %d%% of rows being broken.\n",
+                   "Brokenness can affect the reliability of later tests.\n",
+                   "This is common when character columns are not quoted and then exported to a .CSV.\n",
+                   "Please quote character columns in file '%s.'\n\n",
+                   "The following columns are potential unquoted character columns and may be the source of the break:\n\n%s\n\n",
+                   "Elements in the following rows appear to contain character values without quotes:\n\n%s\n\n\n\n"
+                ),
+                basename(path),
+                round(proportion*100,0),
+                basename(path),
+                paste(paste0("\t- ", cols), collapse = "\n"),
+                output_truncate(offender_rows)
+            )
+
+    } else {
+
+        cols <- NULL
+        offender_rows <- NULL
+        valid <- TRUE
+        error <- NULL
+        proportion <- 0
+        message <- ""
     }
-
-    if (nrow(problem_cases)>0) {
-        offending_problems <- grepl("^\\d+\\s*columns$", problem_cases[["expected"]])
-
-            if (nrow(problem_cases[offending_problems, ])>=nrow(data) ) {
-
-                valid <- FALSE
-                error <- "empty-column"
-                offender_rows <- 1:nrow(data)
-                proportion <- 1 - length(offender_rows)/nrow(data)
-
-    	        message <- sprintf(
-    	            paste0(
-        	           header("Broken CSV Test"),
-                       "The file '%s' appears to be a broken csv by a trailing column being empty.\n",
-                       "Brokenness can affect the reliability of later tests.\n",
-                       "This is common when exported CSVs are manually inspected, introducing changes to original SQL pull.\n\n\n\n"
-    	            ),
-    	            basename(path)
-    	        )
-
-
-            } else {
-
-                cols <- candidates_for_quotes(path)
-                valid <- FALSE
-                error <- "comma-broken"
-                offender_rows <- problem_cases[["row"]][offending_problems]
-                proportion <- 1 - length(offender_rows)/nrow(data)
-
-    	        message <- sprintf(
-        	            paste0(header("Broken CSV Test"),
-                       "The file '%s' appears to be a broken csv with about %d%% of rows being broken.\n",
-                       "Brokenness can affect the reliability of later tests.\n",
-                       "This is common when character columns are not quoted and then exported to a .CSV.\n",
-                       "Please quote character columns in file '%s.'\n\n",
-                       "The following columns are potential unquoted character columns and may be the source of the break:\n\n%s\n\n",
-                       "Elements in the following rows appear to contain character values without quotes:\n\n%s\n\n\n\n"
-                    ),
-                    basename(path),
-                    round(proportion*100,0),
-                    basename(path),
-                    paste(paste0("\t- ", cols), collapse = "\n"),
-                    output_truncate(offender_rows)
-                )
-
-            }
-        } else {
-
-            cols <- NULL
-            offender_rows <- NULL
-            error <- NULL
-            valid <- TRUE
-            proportion <- 0
-            message <- ""
-        }
 
 
 
@@ -130,55 +147,7 @@ print.vf_csv_broken <- function (x, ...)	{
 	}
 }
 
-# #' Validate that a CSV's Is Properly Quoted
-# #'
-# #' \code{comma_broken_report} - Generates accomanying report.
-# #'
-# #' @param x A file or table validation function's (prefixed with \code{vf_} or
-# #' \code{vt_}) output.
-# #' @param \ldots ignored.
-# #' @rdname vf_csv_broken
-# #' @export
-#
-#
-#
-#
-# broken_message <- function(x, ...){
-#
-# 	if (!isTRUE(x[["valid"]])) {#{
-#
-# 	    if (x[["error"]]=="comma-broken"){
-# 	        message <- sprintf( paste0(header("Broken CSV Test"),
-#                "The file '%s' appears to be a broken csv with about %d %% of rows being broken.\n",
-#                "Brokenness can affect the reliability of later tests.\n",
-#                "This is common when character columns are not quoted and exported to a .CSV.\n",
-#                "Please quote character columns in file '%s.'\n\n",
-#                "The following columns are potential unquoted character columns and may be the source of the break:\n\n%s\n\n",
-#                "Elements in the following rows appear to contain character values without quotes:\n\n%s\n\n\n\n"
-#         ),
-#         x[["file_name"]],
-#         round(x[["proportion"]]*100,0),
-#         x[["file_name"]],
-#         paste(paste0("\t- ", x[["locations"]][["columns"]]), collapse = "\n"),
-#         output_truncate(x[["locations"]][["rows"]])
-#     )
-# 	    } else {
-# 	        message <- sprintf(
-# 	            paste0(header("Broken CSV Test"),
-#                "The file '%s' appears to be a broken csv by a trailing column being empty.\n",
-#                "Brokenness can affect the reliability of later tests.\n",
-#                "This is common when exported CSVs are manually inspected, introducing changes to original SQL pull.\n\n\n\n"
-# 	        ),
-# 	        x[["file_name"]]
-# 	        )
-#
-# 	    }
-#
-# 	} else {
-# 		message <- ""
-# 	}
-#
-#     message
-# }
+
+
 
 
